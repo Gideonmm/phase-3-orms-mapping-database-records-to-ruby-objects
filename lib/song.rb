ba2e324ead31d5b84f596f85ecaa8b67dc22ec67
Gideon Mutugi <gideonmutugi24@gmail.com>
2023-05-29 20:02:00 +0300

    completed orms mapping

--- lib/song.rb
+++ lib/song.rb
@@ -37,16 +37,38 @@ class Song
     # insert the song
     DB[:conn].execute(sql, self.name, self.album)
 
-    # get the song ID from the database and save it to the Ruby instance
     self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
 
-    # return the Ruby instance
     self
   end
 
   def self.create(name:, album:)
-    song = Song.new(name: name, album: album)
+    song = Song.new(id: nil, name: name, album: album)
     song.save
   end
 
+  def self.new_from_db(row)
+    id, name, album = row
+    Song.new(id: id, name: name, album: album)
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT * FROM songs
+    SQL
+
+    results = DB[:conn].execute(sql)
+    results.map { |row| new_from_db(row) }
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM songs WHERE name = ?
+    SQL
+
+    result = DB[:conn].execute(sql, name).first
+    new_from_db(result) if result
+  end
+
+  # Rest of the class implementation
 end
